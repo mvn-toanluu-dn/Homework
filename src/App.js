8841859406028520bc84da4bc8974c24b3e41341
Toan Luu Vinh <toan.luu@monstar-lab.com>
2022-04-15 08:45:01 +0700

    ReactDay2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './styles.css';
+import Header from './components/headers/Header';
+import MainPages from './components/mainpages/MainPages';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <MainPages />
+    <Footer />
+  </div>
+  );
+}
+export default App;
+
